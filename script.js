7849ed4592b8255bd2c096ab166d4b8bab61b6b4
Tonit93 <141759801+Tonit93@users.noreply.github.com>
2025-02-23 14:22:15 +0200

    First commit
    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputSearh = document.querySelector('.info-search-input-js')
+
+inputSearh.addEventListener('input', function(userInput) {
+   userInput.target.valeu;
+   console.log(userInput)
+})
